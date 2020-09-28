70dbbc714644ead237b54f79c3d2146adec400a8
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-28 14:21:31 -0500

    Added implementation for word_search__grid__coordinates__translate.

--- libword_search/src/grid_coordinates.c
+++ libword_search/src/grid_coordinates.c
@@ -15,14 +15,85 @@ bool word_search__grid_coordinates__equals(
     return false;
 }
 
-bool word_search__grid_coordinates__translate(
-    WordSearch__GridCoordinates coordinates,
-    WordSearch__GridVector vector,
-    WordSearch__GridCoordinates *out__translated_coordinates
-){
-    (void )( coordinates );
-    (void)( vector );
-    (void)( out__translated_coordinates );
+bool word_search__grid_coordinates__translate( WordSearch__GridCoordinates coordinates, WordSearch__GridVector vector, WordSearch__GridCoordinates *out__translated_coordinates ){
+    switch( vector.direction ){
+        case WordSearch__Direction__East:
+            *out__translated_coordinates = (WordSearch__GridCoordinates) {
+                .row = coordinates.row,
+                .column = coordinates.column + vector.magnitude
+            };
 
-    return false;
+            return true;
+        case WordSearch__Direction__SouthEast:
+            *out__translated_coordinates = (WordSearch__GridCoordinates) {
+                .row = coordinates.row + vector.magnitude,
+                .column = coordinates.column + vector.magnitude
+            };
+
+            return true;
+        case WordSearch__Direction__South:
+            *out__translated_coordinates = (WordSearch__GridCoordinates) {
+                .row = coordinates.row + vector.magnitude,
+                .column = coordinates.column
+            };
+
+            return true;
+        case WordSearch__Direction__SouthWest:
+            if( vector.magnitude > coordinates.column ){
+                return false;
+            }
+
+            *out__translated_coordinates = (WordSearch__GridCoordinates) {
+                .row = coordinates.row + vector.magnitude,
+                .column = coordinates.column - vector.magnitude
+            };
+
+            return true;
+        case WordSearch__Direction__West:
+            if( vector.magnitude > coordinates.column ){
+                return false;
+            }
+
+            *out__translated_coordinates = (WordSearch__GridCoordinates) {
+                .row = coordinates.row,
+                .column = coordinates.column - vector.magnitude
+            };
+
+            return true;
+        case WordSearch__Direction__NorthWest:
+            if( vector.magnitude > coordinates.row || vector.magnitude > coordinates.column ){
+                return false;
+            }
+
+            *out__translated_coordinates = (WordSearch__GridCoordinates) {
+                .row = coordinates.row - vector.magnitude,
+                .column = coordinates.column - vector.magnitude
+            };
+
+            return true;
+        case WordSearch__Direction__North:
+            if( vector.magnitude > coordinates.row ){
+                return false;
+            }
+
+            *out__translated_coordinates = (WordSearch__GridCoordinates) {
+                .row = coordinates.row - vector.magnitude,
+                .column = coordinates.column
+            };
+
+            return true;
+        case WordSearch__Direction__NorthEast:
+            if( vector.magnitude > coordinates.row ){
+                return false;
+            }
+
+            *out__translated_coordinates = (WordSearch__GridCoordinates) {
+                .row = coordinates.row - vector.magnitude,
+                .column = coordinates.column + vector.magnitude
+            };
+
+            return true;
+        default:
+            return false;
+    }
 }
