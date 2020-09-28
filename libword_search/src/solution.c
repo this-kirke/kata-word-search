09db98d95bf471aa95442c96e1a16754f5e12303
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-28 12:51:17 -0500

    Added implementation for method word_search__solution__equals.

--- libword_search/src/solution.c
+++ libword_search/src/solution.c
@@ -5,8 +5,19 @@
 #include "word_search/solution.h"
 
 bool word_search__solution__equals( WordSearch__Solution first, WordSearch__Solution second ){
-    (void)( first );
-    (void)( second );
+    // The two solutions are equal iff fields word and disposition are equal
+    if(
+        string__equals( first.word, second.word ) == 0 ||
+        first.disposition != second.disposition
+    ){
+        return false;
+    }
 
-    return false;
+    // If the word was found, then the sequence fields must also be equal.
+    if( first.disposition == WordSearch__Solution__Disposition__Found ){
+        return word_search__grid_sequence__equals( first.sequence, second.sequence );
+    }
+
+    // The word was not found in either case, so the solutions are equal.
+    return true;
 }
