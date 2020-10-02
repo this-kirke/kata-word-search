576a128c89e65917cb780185a4bef096f1af5b50
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-10-02 12:16:00 -0500

    Add implementation for method word_search__find_word_in_direction.

--- libword_search/src/word_search.c
+++ libword_search/src/word_search.c
@@ -6,10 +6,56 @@ WordSearch__Solution word_search__find_word_in_direction(
     WordSearch__GridHashMap grid_hash_map,
     WordSearch__Direction direction
 ){
-    (void)( word );
-    (void)( grid );
-    (void)( grid_hash_map );
-    (void)( direction );
+    unsigned long long candidate_character_index;
+    List__WordSearch__GridCoordinates *candidates_list;
 
-    return (WordSearch__Solution) {0};
+    if(
+        word_search__grid_hash_map__retrieve_candidate_coordinates(
+            grid_hash_map,
+            word,
+            &candidate_character_index,
+            &candidates_list
+        )
+    ){
+        while( candidates_list != NULL ){
+            WordSearch__GridCoordinates start_coordinates;
+
+            if(
+                // Unlikely
+                word_search__grid_coordinates__translate(
+                    candidates_list->value,
+                    (WordSearch__GridVector) {
+                        .magnitude = candidate_character_index,
+                        .direction = word_search__direction__opposite( direction ),
+                    },
+                    &start_coordinates
+                ) == false
+            ){
+                candidates_list = candidates_list->next;
+                continue;
+            }
+
+            WordSearch__GridSequence sequence = {
+                .start = start_coordinates,
+                .span = {
+                    .magnitude = word.length,
+                    .direction = direction
+                }
+            };
+
+            if( word_search__grid__sequence_matches_word( grid, sequence, word ) ){
+                return (WordSearch__Solution) {
+                    .word = word,
+                    .disposition = WordSearch__Solution__Disposition__Found,
+                    .sequence = sequence
+                };
+            }
+            candidates_list = candidates_list->next;
+        }
+    }
+
+    return (WordSearch__Solution){
+        .word = word,
+        .disposition = WordSearch__Solution__Disposition__NotFound
+    };
 }
